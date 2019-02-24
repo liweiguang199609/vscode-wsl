f394ab3d4c3074d1f18c8d61bf1a1e7a37ddc539
liweiguang199609 <liweiguang199609@users.noreply.github.com>
2019-02-24 19:29:07 +0800

    Add files via upload
    path:/mnt/e/test/test

---
+++ test.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+    printf("success\n");
+    int i = 0;
+    if(i) cout << i << endl;
+    else cout << "i is 0" << endl;
+    return 0;
+}
